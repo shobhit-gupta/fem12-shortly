7397400925575a23ec71dfae4ba66bc9c913c003
Shobhit Gupta <25628519+shobhit-gupta@users.noreply.github.com>
2023-05-22 07:29:29 +0000

    feat: Add slide transition to mobile menu

--- src/lib/UI/Navbar.svelte
+++ src/lib/UI/Navbar.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+	import { slide } from 'svelte/transition';
+
 	export let isMenuOpen: boolean;
+	let innerWidth: number;
 </script>
 
-<nav
-	class:flex={isMenuOpen}
-	class:hidden={!isMenuOpen}
-	class="
+<svelte:window bind:innerWidth />
+
+{#if isMenuOpen || innerWidth >= 768}
+	<nav
+		transition:slide={{ duration: isMenuOpen ? 400 : 0 }}
+		class="
         nav__divider
         nav__body
         nav__text
@@ -16,17 +21,18 @@
         md:items-center
         md:justify-between
     "
->
-	<div class="flex flex-col gap-7 pb-7 md:flex-row md:pb-0">
-		<a href="/">Features</a>
-		<a href="/">Pricing</a>
-		<a href="/">Resources</a>
-	</div>
-	<div class="flex flex-col gap-6 pt-7 md:flex-row md:items-center md:gap-9 md:pt-0">
-		<a href="/">Login</a>
-		<a href="/" class="btn-primary--md md:btn-primary--sm">Sign Up</a>
-	</div>
-</nav>
+	>
+		<div class="flex flex-col gap-7 pb-7 md:flex-row md:pb-0">
+			<a href="/">Features</a>
+			<a href="/">Pricing</a>
+			<a href="/">Resources</a>
+		</div>
+		<div class="flex flex-col gap-6 pt-7 md:flex-row md:items-center md:gap-9 md:pt-0">
+			<a href="/">Login</a>
+			<a href="/" class="btn-primary--md md:btn-primary--sm">Sign Up</a>
+		</div>
+	</nav>
+{/if}
 
 <style lang="postcss">
 	.nav__divider {
