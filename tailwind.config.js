ac89df3e962eb9f0c2d7fea9e5e541384e1f3d2d
Shobhit Gupta <25628519+shobhit-gupta@users.noreply.github.com>
2023-05-23 12:15:34 +0000

    style: Add body background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,8 @@ export default {
 				gray: 'hsl(0, 0%, 75%)',
 				grayishViolet: 'hsl(257, 7%, 63%)',
 				veryDarkBlue: 'hsl(255, 11%, 22%)',
-				veryDarkViolet: 'hsl(260, 8%, 14%)'
+				veryDarkViolet: 'hsl(260, 8%, 14%)',
+				whitesmoke: 'hsl(225, 32%, 95%)'
 			},
 			fontFamily: {
 				sans: ['Poppins', 'sans-serif']
